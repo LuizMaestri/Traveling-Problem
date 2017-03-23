63ad8b04349be8ee3a3fb97121ba60205f9d83c8
LuizMaestri <luizricardomaestri@gmail.com>
2017-03-23 20:06:18 -0300

    Start Main Method

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import domain.Graph;
+
+/**
+ * @author luiz.maestri
+ * @since 23/03/17
+ */
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(args[0]);
+    }
+}
